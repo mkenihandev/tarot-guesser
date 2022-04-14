da24f7ec93f2b1895a196a246af87f31d6c13b31
mkenihandev <mkenihan96@gmail.com>
2022-04-14 19:52:37 +0000

    Add js for basic answer checker

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,22 +2,18 @@ document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByClassName('question');
 
     for (let button of buttons) {
-        button.addEventListener('click', checkAnswer())
+        button.addEventListener('click', function() {
+            checkAnswer(this.innerHTML);
+            console.log(this.innerHTML);
+        })
     }
 
-    runGame(fool);
+    runGame();
 });
 
-function runGame(card) {
-    switch (card) {
-        case 'fool':
-            displayFoolQuestions();
-            break;
-        case 'magician':
-            displayMagicQuestions();
-            break;
-    }
-}
+function runGame() {
+    displayFoolQuestions();
+};
 
 function displayFoolQuestions() {
     document.getElementsByClassName('q1')[0].textContent = 'The Demon';
@@ -25,3 +21,14 @@ function displayFoolQuestions() {
     document.getElementsByClassName('q3')[0].textContent = 'The Hermit';
     document.getElementsByClassName('q4')[0].textContent = 'The Emperor';
 };
+
+function checkAnswer(answerClicked) {
+    let answer = 'The Fool';
+    let correct = answerClicked === answer;
+
+    if (correct) {
+        alert('You did it!');
+    } else {
+        alert('Uh oh stinky!');
+    }
+}
